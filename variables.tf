7fe5bcd4018c060610e53d1c40733c6cd8eca8dd
Martin Yankov <martin.yankov@ship.cars>
2025-12-06 15:46:04 +0200

    fix formatting

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   type        = string
-  description =                     "The name of the resource group."
+  description = "The name of the resource group."
 }
 
 variable "resource_group_location" {
